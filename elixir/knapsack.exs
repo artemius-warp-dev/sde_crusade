6bd5d8e0fecb0d2721cfa04144a5a89f8969316b
a.salagaev <a.salagaev@fun-box.ru>
2023-06-07 21:23:44 +0300

    07062023

--- elixir/knapsack.exs
+++ elixir/knapsack.exs
@@ -4,42 +4,79 @@ defmodule Knapsack do
   """
   @spec maximum_value(items :: [%{value: integer, weight: integer}], maximum_weight :: integer) ::
           integer
-
   def maximum_value(items, maximum_weight) do
-    items
-    |> Enum.sort_by(&({&1.weight, &1.value}), fn {w1,v1},{w2,v2} ->
-      IO.inspect({{w1,v1},{w2, v2}})
-       w1 <= w2
-    end)
-    |> IO.inspect()
-    |> do_greedy_scan(maximum_weight)
+    number_of_items = length(items)
+    dp = get_cache(number_of_items, maximum_weight)
+    res = maximum_value(items, maximum_weight, dp)
+    res[{number_of_items, maximum_weight}]
   end
 
-
-  defp do_greedy_scan(items, maximum_weight) do
-    items
-    |> Enum.reduce({0, maximum_weight}, fn
-      x, {profit, m_weight} when x.weight <= m_weight ->
-        {x.value+profit, m_weight - x.weight}
-      _, acc -> acc
+  defp maximum_value(items, maximum_weight, dp) do
+    Enum.with_index(items, 1)
+    |> Enum.reduce(dp, fn item, dp ->
+      fill_cache(item, 1, maximum_weight, items, dp)
     end)
   end
-  
-  # defp scan([], _), do: []
-  # defp scan(items, maximum_weight) do
-  #   Enum.map(items, fn item -> sum(item.weight, items -- [item], maximum_weight, item.value) end)
-  # end
 
-  # defp sum(_, [], _, acc), do: acc
+  defp fill_cache({item, i}, weight, maximum_weight, items, dp) when weight <= maximum_weight do
+    if pack?({item, i}, weight) do
+      new_value =
+        Enum.max([
+          dp[{i - 1, weight}],
+          dp[{i - 1, weight - item.weight}] + item.value
+        ])
 
-  # defp sum(weight, [h | tail], maximum_weight, acc) do
-  #   if weight + h.weight <= maximum_weight do
-  #     sum(weight + h.weight, tail, maximum_weight, acc + h.value)
-  #   else
-  #     sum(weight, tail, maximum_weight, acc)
-  #   end
-  # end
+      fill_cache(
+        {item, i},
+        weight + 1,
+        maximum_weight,
+        items,
+        Map.put(dp, {i, weight}, new_value)
+      )
+    else
+      fill_cache(
+        {item, i},
+        weight + 1,
+        maximum_weight,
+        items,
+        Map.put(dp, {i, weight}, dp[{i - 1, weight}])
+      )
+    end
+  end
+
+  defp fill_cache(_, _, _, _, dp), do: dp
+
+  defp pack?({item, _i}, capacity) do
+    item.weight <= capacity
+  end
+
+  defp get_cache(items_amount, maximum_weight) do
+    for w <- 0..maximum_weight,
+        i <- 0..items_amount,
+        into: %{},
+        do: {{i, w}, 0}
+  end
 end
+
+# def maximum_value(items, maximum_weight) do
+#   do_max_val(items, maximum_weight, 0)
+# end
+
+# defp do_max_val([%{value: value, weight: weight} | rest], capacity, acc) do
+#   if weight > capacity do
+#     acc
+#   else
+#     with_this = do_max_val(rest, capacity - weight, acc + value)
+
+#     without_this = do_max_val(rest, capacity, acc)
+
+#     max(with_this, without_this)
+#   end
+# end
+
+# defp do_max_val([], _, acc), do: acc
+
+# defp do_max_val(_, 0, acc), do: acc
 
 items = [
   %{value: 350, weight: 25},
@@ -53,6 +90,29 @@ items = [
 ]
 
 maximum_weight = 104
-#assert Knapsack.maximum_value(items, maximum_weight) == 900
+# assert Knapsack.maximum_value(items, maximum_weight) == 900
+
+# items = [
+#   %{value: 5, weight: 2},
+#   %{value: 5, weight: 2},
+#   %{value: 5, weight: 2},
+#   %{value: 5, weight: 2},
+#   %{value: 21, weight: 10}
+# ]
+
+# maximum_weight = 10
+# assert Knapsack.maximum_value(items, maximum_weight) == 21
+
+# items = [
+#   %{value: 20, weight: 2},
+#   %{value: 20, weight: 2},
+#   %{value: 20, weight: 2},
+#   %{value: 20, weight: 2},
+#   %{value: 50, weight: 10}
+# ]
+
+# maximum_weight = 10
+
+# assert Knapsack.maximum_value(items, maximum_weight) == 80
 
 Knapsack.maximum_value(items, maximum_weight) |> IO.inspect()
