7f9196f0ab218fe65959f768e3e96afae7a5f723
Artem Salagaev <artem.salagaev.dev@gmail.com>
2024-11-13 20:11:40 +0500

    improved version with new loop end condition

--- golang/educative/grokking_interview_patterns/two_pointers/valid_palindrom.go
+++ golang/educative/grokking_interview_patterns/two_pointers/valid_palindrom.go
@@ -3,16 +3,13 @@ package main
 func isPalindrome(inputString string) bool {
 	start := 0
 	end := len(inputString) - 1
-	mid := end / 2
-	for {
-		if start == mid || end == mid {
-			return true
-		}
-		if inputString[start] == inputString[end] {
-			start++
-			end--
-		} else {
+	for start < end {
+		if inputString[start] != inputString[end] {
 			return false
 		}
+		start++
+		end--
 	}
+
+	return true
 }
