f91adccfebd3fabb885a6be438837852d1a83ad3
Artem Salagaev <artem.salagaev.dev@gmail.com>
2024-11-13 20:23:49 +0500

    test cases has been added

--- golang/educative/grokking_interview_patterns/two_pointers/valid_palindrom.go
+++ golang/educative/grokking_interview_patterns/two_pointers/valid_palindrom.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 func isPalindrome(inputString string) bool {
 	runes := []rune(inputString)
 	start := 0
@@ -13,4 +15,40 @@ func isPalindrome(inputString string) bool {
 	}
 
 	return true
+}
+
+type TestCase struct {
+	Input string
+	Expected bool
+}
+
+func runTests() {
+	testCases := []TestCase{
+		{"", true},
+		{"a", true},
+		{"racecar", true},               
+		{"hello", false},                
+		{"level", true},                
+		{"😊a😊", true},                
+		{"абба", true},                 
+		{"абвгд", false},               
+		{"A man a plan a canal Panama", false}, 
+		{"あいういあ", true},             
+		{"あいうえお", false},
+	}
+
+
+	for i, testCase := range testCases {
+		result := isPalindrome(testCase.Input)
+		if result == testCase.Expected {
+			fmt.Printf("Test %d passed! Input: '%s' -> Output: %v\n", i+1, testCase.Input, result) 
+		} else {
+			fmt.Printf("Test %d failed! Input: '%s' -> Expected: %v, Got: %v\n", i+1, testCase.Expected, result)
+		}
+	}
+}
+
+
+func main() {
+	runTests()
 }
