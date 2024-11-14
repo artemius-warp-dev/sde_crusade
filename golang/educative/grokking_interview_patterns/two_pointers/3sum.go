17af7abc14f2c9282f245b024f1aab59a2aaff1f
Artem Salagaev <artem.salagaev.dev@gmail.com>
2024-11-14 22:31:57 +0500

    A little bit improved version has been added

--- golang/educative/grokking_interview_patterns/two_pointers/3sum.go
+++ golang/educative/grokking_interview_patterns/two_pointers/3sum.go
@@ -3,13 +3,14 @@ package main
 import (
 	"fmt"
 	"sort"
+	"strings"
 )
 
 func findSumOfThree(nums []int, target int) bool {
 	sort.Ints(nums)
 	fmt.Println(nums)
 
-	for i := 0; i < len(nums); i++ {
+	for i := 0; i < len(nums)-2; i++ {
 		left := i + 1
 		right := len(nums) - 1
 		for left < right {
@@ -27,8 +28,24 @@ func findSumOfThree(nums []int, target int) bool {
 	return false
 }
 
+// Driver code
 func main() {
-	input := []int{3, 4, 1, 0, 4, 1, 2, 2, 6}
-	result := findSumOfThree(input, 15)
-	fmt.Println(result)
+	numsLists := [][]int{
+		{3, 7, 1, 2, 8, 4, 5},
+		{-1, 2, 1, -4, 5, -3},
+		{2, 3, 4, 1, 7, 9},
+		{1, -1, 0},
+		{2, 4, 2, 7, 6, 3, 1},
+	}
+	tList := []int{10, 7, 20, -1, 8}
+	for i, nList := range numsLists {
+		fmt.Printf("%d. Input array: %s\n", i+1, strings.Replace(fmt.Sprint(nList), " ", ", ", -1))
+
+		if findSumOfThree(nList, tList[i]) {
+			fmt.Printf("   Sum for %d exists\n", tList[i])
+		} else {
+			fmt.Printf("   Sum for %d does not exist\n", tList[i])
+		}
+		fmt.Printf("%s\n", strings.Repeat("-", 100))
+	}
 }
