7986c89fd21dbbd66e666e39ff644d5d37bf348e
Artem Salagaev <artem.salagaev.dev@gmail.com>
2024-11-15 23:00:21 +0500

    Remove nth node draft

---
+++ golang/educative/grokking_interview_patterns/two_pointers/remove_nth_node.go
@@ -0,0 +1,8 @@
+package main
+
+func removeNthLastNode(head *EduLinkedListNode, n int) *EduLinkedListNode {
+
+	// Replace this placeholder return statement with your code
+
+	return new(EduLinkedListNode)
+}
