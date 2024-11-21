66b3499ccc7413d7740b684490d83b24f74302ad
Artem Salagaev <artem.salagaev.dev@gmail.com>
2024-11-21 19:50:41 +0500

    Improved version of abbreviation validation has been added

--- golang/educative/grokking_interview_patterns/two_pointers/valid_word_abbreviation.go
+++ golang/educative/grokking_interview_patterns/two_pointers/valid_word_abbreviation.go
@@ -2,49 +2,45 @@ package main
 
 import (
 	"fmt"
-	"strconv"
-	"unicode"
+	"strings"
 )
 
 func validWordAbbreviation(word string, abbr string) bool {
-	rWord := []rune(word)
-	rAbbr := []rune(abbr)
-	wordPtr, abbrPtr := 0, 0
-	strNumber := ""
+	wordIndex := 0
+	abbrIndex := 0
 
-	for abbrPtr < len(rAbbr) {
-		r := rAbbr[abbrPtr]
-		fmt.Printf("%c  %d, %d \n", r, abbrPtr, wordPtr)
-		if unicode.IsDigit(r) {
-			for unicode.IsDigit(rAbbr[abbrPtr]) {
-				strNumber += string(rAbbr[abbrPtr])
-				abbrPtr++
-			}
-			fmt.Println(strNumber)
-			number, _ := strconv.Atoi(strNumber)
-			wordPtr += number
-
-		} else {
-			strNumber = ""
-			fmt.Printf("CHARACTER: %c, %d, %c\n", r, wordPtr, rWord[wordPtr])
-			if r != rWord[wordPtr] {
+	for abbrIndex < len(abbr) {
+		if '0' <= abbr[abbrIndex] && abbr[abbrIndex] <= '9' {
+			if abbr[abbrIndex] == '0' {
 				return false
 			}
-			wordPtr++
-			abbrPtr++
-		}
+			num := 0
 
-		if abbrPtr == len(rAbbr)-1 && wordPtr == len(rWord)-1 {
-			return true
+			for abbrIndex < len(abbr) && '0' <= abbr[abbrIndex] && abbr[abbrIndex] <= '9' {
+				num = num*10 + int(abbr[abbrIndex]-'0')
+				abbrIndex++
+			}
+			wordIndex += num
+		} else {
+			if wordIndex >= len(word) || word[wordIndex] != abbr[abbrIndex] {
+				return false
+			}
+			wordIndex++
+			abbrIndex++
 		}
 	}
-	fmt.Println("END")
-	return false
+
+	return wordIndex == len(word) && abbrIndex == len(abbr)
 }
 
 func main() {
-	//word, abbr := "physiotherapist", "3sio5pist"
-	word, abbr := "internationalization", "13iz4n"
-	fmt.Println(validWordAbbreviation(word, abbr))
+	words := []string{"a", "a", "abcdefghijklmnopqrst", "abcdefghijklmnopqrst", "word", "internationalization", "localization"}
+	abbreviations := []string{"a", "b", "a18t", "a19t", "w0rd", "i18n", "l12n"}
 
+	for i, word := range words {
+		fmt.Printf("%d.\t word: '%s'\n", i+1, word)
+		fmt.Printf("\t abbr: '%s'\n", abbreviations[i])
+		fmt.Printf("\n\t Is '%s' a valid abbreviation for the word '%s'? %v\n", abbreviations[i], word, validWordAbbreviation(word, abbreviations[i]))
+		fmt.Println(strings.Repeat("-", 100))
+	}
 }
